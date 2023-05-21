6a372fc08af3c999379cdd2974cae44cf934ec9f
Borja Sahuquillo <sahuquillo.borja@gmail.com>
2023-05-21 13:10:25 +0200

    finish insert seed data

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,65 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  // With only one request for all pokemons
   async executeSeed() {
+    await this.pokemonModel.deleteMany(); // delete * from pokemons;
+
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
+
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no });
       console.log(name, no);
     });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
     return data.results;
   }
+
+  /**
+   * With one request for pokemon with promiseALl. Not recommended
+  async executeSeed() {
+    await this.pokemonModel.deleteMany(); // delete * from pokemons;
+
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    const insertPromisesArray = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      insertPromisesArray.push(this.pokemonModel.create({ name, no }));
+
+      console.log(name, no);
+    });
+
+    await Promise.all(insertPromisesArray);
+
+    return data.results;
+  }
+   **/
 }
